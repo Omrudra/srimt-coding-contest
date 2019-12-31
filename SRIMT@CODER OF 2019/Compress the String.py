33225b843ad38f541c435a67ff3d3db0d8cbe641
Sunil Kumar Yadav <52737895+Omrudra@users.noreply.github.com>
2020-01-01 01:23:49 +0530

    Create Compress the String.py

---
+++ SRIMT@CODER OF 2019/Compress the String.py
@@ -0,0 +1,20 @@
+def compress(string):
+
+    res = ""
+    count = 1
+    res += string[0]
+
+    #Iterate through loop, skipping last one
+    for i in range(len(string)-1):
+        if(string[i] == string[i+1]):
+            count+=1
+        else:
+            if(count > 1):
+                res += str(count)
+            res += string[i+1]
+            count = 1
+    if(count > 1):
+        res += str(count)
+    print( res)
+string=input()
+compress(string)
